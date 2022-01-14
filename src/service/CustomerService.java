f05cd79bdf7f6e4065520bdcf57060706e6e59bf
Ken Wu <kenwu1009us@gmail.com>
2022-01-13 17:38:14 -0800

    upload CustomerService.java

---
+++ src/service/CustomerService.java
@@ -0,0 +1,28 @@
+package service;
+
+import model.Customer;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ken Wu
+ *
+ */
+public class CustomerService {
+
+    private final Map<String, Customer> customers = new HashMap<>();
+
+    public void addCustomer(String email, String firstName, String lastName) {
+        customers.put(email, new Customer(firstName, lastName, email));
+    }
+
+    public Customer getCustomer(String customerEmail) {
+        return customers.get(customerEmail);
+    }
+
+    public Collection<Customer> getAllCustomers() {
+        return customers.values();
+    }
+}
